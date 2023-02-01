e2387fe5be433e1abb0414efe4e10c867ca2bfe8
Islan Victhor <islan12victhor@gmail.com>
2023-02-01 06:48:52 -0300

    Minicode adicionado ao mainjs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-const startBtn = document.getElementById("start-game")
+import MiniCode from "./modules/minicode.js"
+//const startBtn = document.getElementById("start-game")
 const numSelect = document.getElementById("num-select")
 const form = document.getElementById("form")
+const panel = document.getElementById("game-panel")
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
-  console.log(numSelect.value)
+  while (panel.firstChild) {
+    panel.removeChild(panel.firstChild)
+  }
+  for (let i = 0; i < numSelect.value; i ++) {
+    const gamePanel = document.createElement("div")
+    panel.appendChild(gamePanel)
+    const minigame = new MiniCode(gamePanel)
+  }
 })
