b175cc69fd8f4d7e18ae18a89288a2a9d23cf94f
Islan Victhor <islan12victhor@gmail.com>
2023-07-29 16:01:39 -0300

    const animate
    Created and exported the const animate wich store methods used by the main.animation() method.

--- modules/minicode.js
+++ modules/minicode.js
@@ -1,4 +1,4 @@
-import {teste} from "./utils.js"
+import {teste, animate} from "./utils.js"
 
 export default class MiniCode {
   root;
@@ -575,270 +575,26 @@ export default class MiniCode {
     
     if(this.gameOn) {
       this.checkBoost();
-      drawDesk.call(this);
-      drawLorem.call(this);
-      drawBars.call(this);
+      animate.drawDesk.call(this);
+      animate.drawLorem.call(this); //
+      animate.drawBars.call(this); //
       if (this.desktop) {
-        drawToPress.call(this);
+        animate.drawToPress.call(this); //
       }
-      drawPenalties.call(this);
+      animate.drawPenalties.call(this); //
       if (this.bugsON.end) {
-        drawBugs.call(this)
+        animate.drawBugs.call(this) //
       }
     } else {
-      drawGameOver.call(this)
+      animate.drawGameOver.call(this) //
     }
-
-    function drawDesk() {
-      this.ctx.save() // SAVE 01: DESK
-      if (this.negativeAnim) {
-        let randX = this.random(-10,10)
-        let randY = this.random(-10,10)
-        this.ctx.translate(randX,randY)
-      }
-      
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      this.ctx.drawImage(this.assets.desk, 0, 0, this.canvas.width, this.canvas.height);
-      
-    }
-    function drawLorem() {
-      const bgColor = this.lvl.maxLvl < 10 ? "#ededed" : "#272822";
-      const fgColor = this.lvl.maxLvl < 10 ? "#000000" : "#ededed";
-      const width = this.canvas.width
-      
-      const dWidth = width / (500/200)
-      const dHeight = width / (500/84)
-      const dX = (width - dWidth)/2
-      const dY = width / (500/33)
-      
-      this.ctx.drawImage(
-        this.assets.lorem,
-        0,
-        21 * this.spriteLorem + 1,
-        200,
-        83,
-        dX,
-        dY,
-        dWidth,
-        dHeight
-      );
-      
-      this.ctx.save()
-      this.ctx.globalCompositeOperation = "source-atop"
-      this.ctx.fillStyle = fgColor;
-      this.ctx.fillRect(dX,dY,dWidth,dHeight)
-      this.ctx.restore()
-      
-      if (this.bugsON.end) {
-        this.ctx.save()
-        this.ctx.globalCompositeOperation = "source-atop"
-        this.ctx.fillStyle = "#f4192c"
-        this.ctx.fillRect(dX,dY,dWidth,dHeight)
-        this.ctx.restore()
-      }
-      
-      this.ctx.save()
-      this.ctx.fillStyle = bgColor;
-      this.ctx.globalCompositeOperation = "destination-atop"
-      this.ctx.fillRect(0,0, this.canvas.width, this.canvas.height)
-      this.ctx.restore()
-      
-      this.ctx.restore() // RESTORE 01: DESK
-      
-      if (this.controlFR % 13 === 0) {
-        if (!(this.spriteLorem % 3 === 0)) {
-          this.spriteLorem++;
-        } else if (this.spriteLorem === 18) {
-          this.spriteLorem = 1;
-        }
-      }
-    }
-    function drawBars() {
-      this.ctx.save()
-      const width = this.canvas.width
-      
-      //CODING
-      const dWidth = width/(500/100)
-      const dHeight = width/(500/13)
-      const dY = width/(500/25)
-      
-      const diference = this.lvl.nextLvl - this.lvl.prevLvl;
-      const percentage = ((this.coding - this.lvl.prevLvl) / diference);
-      this.ctx.fillStyle = "rgb(0,0,0)";
-      this.ctx.fillRect(5, dY - 2, dWidth + 4, dHeight + 4);
-      if (percentage >= 0) {
-        this.ctx.fillStyle = "rgb(127, 127, 227)";
-        this.ctx.fillRect(7, dY, percentage * dWidth, dHeight);
-      }
-
-      // LVL
-      const font = width/(500/18);
-      this.ctx.font = `${font}px sans-serif`;
-      this.ctx.fillStyle = "rgb(0,0,0)";
-      const lvlText = this.bugsON.end
-        ? `BUGS LVL: ${this.bugsLvl.lvl - 1}`
-        : `LVL: ${this.lvl.lvl}`;
-      this.ctx.fillText(lvlText, 5, width/(500/60));
-
-      // SCORE
-      this.ctx.fillText(`SCORE: ${this.score}`, 5, width/(500/80));
-
-      //BOOST
-      const boostText =
-        this.bugsON.end
-          ? `${this.pointsMulti}x ${this.boostMulti.toFixed(1)}`
-          : `${this.boostMulti.toFixed(1)}`;
-      this.ctx.fillText(boostText, 5, width/(500/100));
-      
-      this.ctx.restore()
-    }
-    function drawToPress() {
-      const width = this.canvas.width
-      const height = this.canvas.height
-       
-      const dHeight = width / (500/52)
-      const dY = height - dHeight - 5
-      if (this.keytopress === " ") {
-        const dWidth = width / (500/150)
-        const dX = width / 2 - dWidth/2
-        this.ctx.drawImage(this.assets.spacebar, 0, 27 * this.spriteKey, 75, 26, dX, dY, dWidth,dHeight )
-      } else {
-        const dWidth = width / (500/50)
-        const dX = width / 2 - dWidth/2
-        this.ctx.drawImage(
-        this.assets.key,
-        26 * this.spriteKey,
-        0,
-        25,
-        26,
-        dX,
-        dY,
-        dWidth,
-        dHeight
-      );
-      }
-      
-      this.ctx.save();
-      this.ctx.fillStyle = "rgb(255,0,0)";
-      const font = width < 500 ? 16 : 18;
-      this.ctx.font = `bold ${font}px sans-serif`;
-      this.ctx.textAlign = "center"
-      const keyText =
-        this.keytopress === " " ? "SPACEBAR" : `${this.keytopress}`;
-      this.ctx.fillText(
-        keyText,
-        width/(500/250) - 7 * this.spriteKey,
-        width/(500/169) + 6 * this.spriteKey
-      );
-      this.ctx.restore();
-
-      if (this.controlFR % 31 === 0) {
-        if (this.spriteKey === 1) {
-          this.spriteKey = 0;
-        } else {
-          this.spriteKey++;
-        }
-      }
-    }
-    function drawPenalties() {
-      const width = this.canvas.width
-      const tlX = width/(500/470)
-      const tlY = width/(500/5)
-      const size = width/(500/21)
-      if(this.penalties === 1){
-        this.ctx.save()
-        this.ctx.translate(tlX,tlY)
-        this.ctx.rotate(Math.PI/4)
-        this.ctx.drawImage(this.assets.penalties, 21 * this.spritePen,0,21,21,0,0,size,size)
-        this.ctx.restore()
-        
-        if (this.controlFR % 21 === 0) {
-          if (this.spritePen !== 6) {
-            this.spritePen++;
-          }
-        }
-      }
-    }
-    function drawBugs() {
-      const width = this.canvas.width
-      if (this.bugsCount.length) {
-        for (let bug of this.bugsCount) {
-          this.ctx.save()
-          this.ctx.translate(bug.posX,bug.posY)
-          this.ctx.rotate(bug.rads)
-          this.ctx.drawImage(this.assets.bugs, 21 * bug.sprite, 0, 21, 21, 0, 0, bug.size, bug.size)
-          this.ctx.restore()
-        }
-        if (this.controlFR % 61 === 0) {
-          for (let i = 0; i < this.bugsCount.length; i ++) {
-            const rads = this.random(0, Math.PI * 2)
-            const sprite = this.random(0,4)
-            const size = this.random (width/(500/15),width/(500/42))
-            const posX = this.random(0, this.canvas.width - size/2)
-            const posY = this.random(0, this.canvas.height - size/2)
-            
-            this.bugsCount[i] = { posX, posY, rads, sprite, size };
-          }
-        }
-      }
-      
-      if (this.controlFR % 60 === 0 && this.bugsCount.length < 30) {
-        const posX = this.random(0, this.canvas.width)
-        const posY = this.random(0, this.canvas.height)
-        const rads = this.random(0, Math.PI * 2)
-        const sprite = this.random(0,4)
-        const size = this.random (width/(500/15),width/(500/42))
-        
-        const newBug = { posX, posY, rads, sprite, size }
-        this.bugsCount.push(newBug)
-      }
-    }
-    function drawGameOver() {
-      const width = this.canvas.width
-      const ctx = this.ctx;
-      ctx.fillStyle = "rgba(0,0,0,0.7)";
-      ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
-      ctx.save();
-      const a = width/(500/100)
-      const b = width/(500/25)
-      const c = width/(500/300)
-      const d = width/(500/150)
-      ctx.lineWidth = 5;
-      ctx.fillStyle = "rgb(220,220,220)";
-      ctx.fillRect(a, b, c, d);
-      ctx.strokeRect(a, b, c, d);
-      ctx.restore();
-
-      ctx.save();
-      ctx.fillStyle = "rgb(220,0,0)";
-      let font = width/(500/30)
-      ctx.font = `bold ${font}px sans-serif`;
-      const measureGOX = ctx.measureText("GAME OVER");
-      const posGOX = width/2 - measureGOX.width/2;
-      
-      ctx.fillText("GAME OVER", posGOX, width/(500/80));
-      font = width/(500/18)
-      ctx.font = `bold ${font}px sans-serif`;
-      const measureLvlX = ctx.measureText(`LVL: ${this.lvl.maxLvl}`);
-      const posLvlX = width/2 - measureLvlX.width/2;
-      ctx.fillText(`LVL: ${this.lvl.maxLvl}`, posLvlX, width/(500/105));
-      const measureScoreX = ctx.measureText(`SCORE: ${this.score}`);
-      const posScoreX = width/2 - measureScoreX.width/2;
-      ctx.fillText(`SCORE: ${this.score}`, posScoreX, width/(500/130));
-      
-      if (this.controlFR % 31 === 0) {
-        if (this.spriteRestart === 1) {
-          this.spriteRestart = 0
-        } else {
-          this.spriteRestart = 1
-        }
-      }
-      const measureRestartX = ctx.measureText(`RESTART`);
-      const posRestartX = width/2 - measureRestartX.width/2;
-      ctx.fillText(`RESTART`, posRestartX + 3 * this.spriteRestart, width/(500/155) - 3 * this.spriteRestart);
-      ctx.restore();
-    }
+    
+    
+    
+    
+    
+    
+    
   }
   
   deleteThis(){
